50bf4e448dfd708a19904566d77ff50deee9ffde
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-25 18:41:58 -0500

    Finished Problem 5

--- set2.js
+++ set2.js
@@ -57,44 +57,79 @@
 // decode('droop');
 
 //4
-const monthDays = function(month){
-  switch(month) {
-  case 'January':
-    console.log('January has 31 Days');
-    break;
-  case 'February':
-    console.log('February has 28 Days Sometimes');
-    break;
-  case 'March':
-    console.log('March has 31 Days');
-    break;
-  case 'April':
-    console.log('April has 28 Days');
-    break;
-  case 'May':
-    console.log('May has 31 Days');
-    break;
-  case 'June':
-    console.log('June has 30 Days');
-    break;
-  case 'July':
-    console.log('July has 31 Days');
-    break;
-  case 'August':
-    console.log('August has 31 Days');
-    break;
-  case 'September':
-    console.log('September has 30 Days');
-    break;
-  case 'October':
-    console.log('October has 31 Days');
-    break;
-  case 'November':
-    console.log('November has 30 Days');
-    break;
-  case 'December':
-    console.log('Decemeber has 31 Days');
-    break;
+// const monthDays = function(month){
+//   switch(month) {
+//   case 'January':
+//     console.log('January has 31 Days');
+//     break;
+//   case 'February':
+//     console.log('February has 28 Days Sometimes');
+//     break;
+//   case 'March':
+//     console.log('March has 31 Days');
+//     break;
+//   case 'April':
+//     console.log('April has 28 Days');
+//     break;
+//   case 'May':
+//     console.log('May has 31 Days');
+//     break;
+//   case 'June':
+//     console.log('June has 30 Days');
+//     break;
+//   case 'July':
+//     console.log('July has 31 Days');
+//     break;
+//   case 'August':
+//     console.log('August has 31 Days');
+//     break;
+//   case 'September':
+//     console.log('September has 30 Days');
+//     break;
+//   case 'October':
+//     console.log('October has 31 Days');
+//     break;
+//   case 'November':
+//     console.log('November has 30 Days');
+//     break;
+//   case 'December':
+//     console.log('Decemeber has 31 Days');
+//     break;
+//   }
+// };
+// monthDays('March');
+
+//5
+const Rock = 1;
+const Paper = 2;
+const Scissors = 3;
+
+
+//Computer's Code
+function RockPaperScissors(userChoice){
+  let computer = 1 + Math.floor((Math.random()*3));
+  if (userChoice == 1 && computer == 3) {
+    console.log('You Win');
   }
-};
-monthDays('March');
+  else if (userChoice == 1 && computer == 2) {
+    console.log('You Lose');
+  }
+  else if (userChoice == 2 && computer == 1) {
+    console.log('You Win');
+  }
+  else if (userChoice == 2 && computer == 3) {
+    console.log('You Lose');
+  }
+  else if (userChoice == 3 && computer == 1) {
+    console.log('You Lose');
+  }
+  else if (userChoice == 3 && computer == 2) {
+    console.log('You Win');
+  }
+  else {
+    console.log('You Tied');
+  }
+  console.log(computer);
+}
+
+RockPaperScissors(2);
