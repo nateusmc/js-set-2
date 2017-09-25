2af1e18262f9caeff67fe00d666c662386588571
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-25 18:04:28 -0500

    solve problem 3

--- set2.js
+++ set2.js
@@ -27,3 +27,31 @@ beyond(Infinity);
 beyond(4);
 beyond(-4);
 beyond(0);
+
+//3
+
+let decode = function(encodedWord){
+  if (encodedWord.charAt(0) === 'a') {
+    console.log(encodedWord.charAt(1));
+  }
+  else if (encodedWord.charAt(0) === 'b') {
+    console.log(encodedWord.charAt(2));
+  }
+  else if (encodedWord.charAt(0) === 'c') {
+    console.log(encodedWord.charAt(3));
+  }
+  else if (encodedWord.charAt(0) === 'd') {
+    console.log(encodedWord.charAt(4));
+  }
+  else {
+    console.log(' ');
+  }
+};
+decode('craft');
+decode('block');
+decode('argon');
+decode('meter');
+decode('bells');
+decode('brown');
+decode('croon');
+decode('droop');
