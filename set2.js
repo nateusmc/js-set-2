5555ce30fc5cc16def8173673298e373d5a44564
edemahruh <dev@jesseparrott.com>
2017-09-25 16:26:49 -0500

    infinity problem

--- set2.js
+++ set2.js
@@ -1,8 +1,29 @@
-'use strict'
+'use strict';
 //1
 let jediName = function(firstName, lastName) {
   console.log(lastName.slice(0, 3) + firstName.slice(0, 2));
-}
+};
 jediName('Nate', 'Garrett');
 
 //2
+
+let beyond = function (num){
+  if (num === Infinity) {
+    console.log('And beyond');
+  }
+  else if(num > 0 && num !== Infinity) {
+    console.log('To infinity');
+  }
+
+  else if (num < 0 && num !== Infinity){
+    console.log('To negative infinity');
+  }
+
+  else {
+    console.log('Staying home');
+  }
+};
+beyond(Infinity);
+beyond(4);
+beyond(-4);
+beyond(0);
